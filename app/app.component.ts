e63a5f3eef76677a97a482e245a44d79fa524674
alexzuza <alexzuza@yandex.ru>
2016-11-26 17:16:34 +0400

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component }   from '@angular/core';
+
+@Component({
+  selector: 'app-component',
+  template: '<h1>{{title}}</h1>'
+})
+export class AppComponent {
+  title = 'Test app component';
+}
